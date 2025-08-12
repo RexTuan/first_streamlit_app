a5fe8e49007d6368a026b7287d382473d23a0a27
Rex Tuan <roarmosquito@gmail.com>
2025-08-12 17:12:07 +0800

    add #004-understand about "rerun"

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,3 +18,10 @@ else:
 st.title('嗨你好，這是我的第一個streamlit')
 x = st.slider('選擇一個數值')
 st.write(f'{x}的平方是{x**2}')
+
+## 004-理解(rerun)機制>>>只要有跟小工具互動就會重新執行(eg.如拖動滑桿、點擊按鈕或在輸入框中輸入文字。)
+count = 0
+increment = st.button('增加')
+if increment:
+    count += 1
+st.write(f'計數 = {count}')
