dfbfe0fdebd14b2a94a5119ed6411fb519166612
Rex Tuan <roarmosquito@gmail.com>
2025-08-12 15:57:08 +0800

    add show button and if fuction

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,12 @@
 import streamlit as st
 
+# 001-單純顯示文字 
 st.write('Hello World!')
-st.write('test')
+
+# 002-顯示按紐+if功能
+st.header('st.button')
+
+if st.header('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
